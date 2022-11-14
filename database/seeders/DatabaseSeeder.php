dc04554079f006a6e63733cd60f54749d914f322
Fikkie007 <fikkieakbar48@gmail.com>
2022-11-14 11:51:00 +0700

    Fix After Error

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+
+        Post::factory(20)->create();
+    }
+}
---
+++ resources/views/categories.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('container')
+<h1 class="mb-5">Post Categories</h1>
+
+<div class="container">
+    <div class="row">
+        @foreach ($categories as $p)
+
+        <div class="col-md-4">
+            <div class="card bg-dark text-white">
+                <img src="https://source.unsplash.com/500x500?{{ $p->name }}" class="card-img" alt="{{ $p->name }}">
+                <a class="text-decoration-none text-white" href="/blog?category={{$p->slug}}" >
+                <div class="card-img-overlay d-flex align-items-center p-0">
+                  <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.7)">{{ $p->name }}</h5>
+                
+                </div>
+            </a>
+              </div>
+        </div>
+
+        @endforeach 
+    </div>
+</div>
+
+<br><br>
+<a href="/blog">Back To Post</a>
+
+@endsection
